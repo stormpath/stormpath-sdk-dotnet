fc7b4e2c960efbefb31d544d98fae5cb1992464e
nbarbettini <nate@stormpath.com>
2015-07-30 15:05:06 -0700

    reorganized files

---
+++ Stormpath.SDK/Stormpath.SDK/Api/IApiKey.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stormpath.SDK.Api
+{
+    public interface IApiKey
+    {
+        Task<string> GetIdAsync();
+
+        Task<string> GetSecretAsync();
+
+        // ApiKeyStatus
+
+        //void SetStatus()
+
+        //Task<IAccount> 
+
+        //Task<ITenant>
+
+        // void Save()
+    }
+}
---
+++ Stormpath.SDK/Stormpath.SDK/Client/IClientBuilder.cs
@@ -0,0 +1,15 @@
+﻿namespace Stormpath.SDK.Client
+{
+    public interface IClientBuilder
+    {
+        IClientBuilder SetApiKey(ApiKey apiKey);
+
+        IClientBuilder SetAuthenticationScheme(AuthenticationScheme authenticationScheme);
+
+        IClientBuilder SetConnectionTimeout(int timeout);
+
+        IClientBuilder SetBaseUrl(string baseUrl);
+
+        IClient Build();
+    }
+}
