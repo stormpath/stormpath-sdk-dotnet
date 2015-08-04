6ebaf0e245f74cf3f45545d6dee7dff4b4ec899e
Nate Barbettini <nathanaelb@gmail.com>
2015-08-03 17:11:48 -0700

    Added demo project

---
+++ Stormpath.SDK/Stormpath.Demo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Stormpath.SDK.Api;
+
+namespace Stormpath.Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var apiKey = ClientApiKeys.Builder()
+                .SetId("myID")
+                .SetSecret("secret")
+                .Build();
+
+            
+        }
+    }
+}
