f9470af387553a8d64d20151219bbc15cfb1be75
Nate Barbettini <nathanaelb@gmail.com>
2015-08-13 11:18:47 -0700

    Add xunit project

---
+++ Stormpath.SDK/Stormpath.SDK.Tests/Class1.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+
+namespace Stormpath.SDK.Tests
+{
+    public class XunitTester
+    {
+        [Fact]
+        public void AssertBool()
+        {
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void AssertString()
+        {
+            Assert.NotEqual("foo", "bar");
+        }
+    }
+}
