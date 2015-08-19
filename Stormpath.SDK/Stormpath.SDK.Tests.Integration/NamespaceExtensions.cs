e5f64029ec7f045eca72f53dab66e5294e249173
Nate Barbettini <nathanaelb@gmail.com>
2015-08-19 11:22:49 -0700

    Added integration test sanity check that Impl is internal

---
+++ Stormpath.SDK/Stormpath.SDK.Tests.Integration/NamespaceExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stormpath.SDK.Tests.Integration
+{
+    public static class NamespaceExtensions
+    {
+        public static IEnumerable<Type> GetAllTypesInNamespace(this Assembly assembly, string namespaceRoot, bool onlyPublic = true)
+        {
+            var types = assembly.GetTypes()
+                .Where(x => x.Namespace.StartsWith(namespaceRoot, StringComparison.OrdinalIgnoreCase));
+            if (onlyPublic)
+                types = types.Where(x => x.IsPublic);
+
+            foreach (var type in types)
+                yield return type;
+        }
+    }
+}
