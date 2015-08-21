cf98ef31966530399f9e029ac8097351c848474c
Nate Barbettini <nathanaelb@gmail.com>
2015-08-21 10:38:32 -0700

    Let's try xUnit 2.0!

---
+++ Stormpath.SDK/Stormpath.SDK.Tests/Theory_tests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Shouldly;
+using Xunit;
+
+namespace Stormpath.SDK.Tests
+{
+    public class Theory_tests
+    {
+        [Theory]
+        [InlineData("test", 4)]
+        [InlineData("abcde", 5)]
+        [InlineData("ab", 2)]
+        public void String_length_theory(string test, int length)
+        {
+            var result = test.Length;
+
+            result.ShouldBe(length);
+        }
+    }
+}
