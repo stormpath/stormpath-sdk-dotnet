86ab8d830e381c81c1a1b8a38a5fabfec7e6db26
Nate Barbettini <nathanaelb@gmail.com>
2015-09-16 10:37:46 -0700

    DataStore API docs (#14)

--- Stormpath.SDK/Stormpath.SDK/DataStore/IDataStore.cs
+++ Stormpath.SDK/Stormpath.SDK/DataStore/IDataStore.cs
@@ -22,11 +22,32 @@ using Stormpath.SDK.Resource;
 
 namespace Stormpath.SDK.DataStore
 {
+    /// <summary>
+    /// A <see cref="IDataStore"/> is the liaison between client SDK components and the raw Stormpath REST API.
+    /// It is responsible for converting SDK objects (<see cref="Account.IAccount"/>, <see cref="Application.IApplication"/>, <see cref="Directory.IDirectory"/>, etc.)
+    /// into REST HTTP requests, executing those requests, and converting REST HTTP responses back into SDK objects.
+    /// </summary>
     public interface IDataStore : IDisposable
     {
+        /// <summary>
+        /// Instantiates and returns a new instance of the specified <see cref="IResource"/> type.
+        /// The instance is merely instantiated and is not saved/synchronized with the server in any way.
+        /// <para>This method effectively replaces the <c>new</c> keyword that would have been used otherwise if the
+        /// concrete implementation was known (Resource implementation classes are intentionally not exposed to SDK end-users).</para>
+        /// </summary>
+        /// <typeparam name="T">The <see cref="IResource"/> type to instantiate.</typeparam>
+        /// <returns>A new instance of the specified resource type</returns>
         T Instantiate<T>()
             where T : IResource;
 
+        /// <summary>
+        /// Retrieves the resource at the specified <paramref name="href"/> URL and returns the resource
+        /// as an instance of the specified class <typeparamref name="T"/>.
+        /// </summary>
+        /// <typeparam name="T">The type of the returned <see cref="IResource"/> value.</typeparam>
+        /// <param name="href">The resource URL of the resource to retrieve.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns>An instance of the specified class based on data returned from the specified <paramref name="href"/> URL.</returns>
         Task<T> GetResourceAsync<T>(string href, CancellationToken cancellationToken = default(CancellationToken));
     }
 }
