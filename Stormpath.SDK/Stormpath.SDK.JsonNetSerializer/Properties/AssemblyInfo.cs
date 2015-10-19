2f2d35100a022d70a193210ebf8c4d6cb8a0f4e1
Nate Barbettini <nathanaelb@gmail.com>
2015-10-19 12:27:20 -0700

    Version bump for 0.2 release

--- Stormpath.SDK/Stormpath.SDK.JsonNetSerializer/Properties/AssemblyInfo.cs
+++ Stormpath.SDK/Stormpath.SDK.JsonNetSerializer/Properties/AssemblyInfo.cs
@@ -22,11 +22,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Stormpath.SDK.JsonNetSerializer")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("The Official Stormpath SDK for .NET")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Stormpath.SDK.JsonNetSerializer")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCompany("Stormpath, Inc.")]
+[assembly: AssemblyProduct("Stormpath .NET SDK")]
+[assembly: AssemblyCopyright("Copyright © 2015 Stormpath, Inc.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -48,5 +48,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.2.0.0")]
+[assembly: AssemblyFileVersion("0.2.0.0")]
--- Stormpath.SDK/Stormpath.SDK.RestSharpClient/Properties/AssemblyInfo.cs
+++ Stormpath.SDK/Stormpath.SDK.RestSharpClient/Properties/AssemblyInfo.cs
@@ -23,11 +23,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Stormpath.SDK.RestSharpClient")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("The Official Stormpath SDK for .NET")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Stormpath.SDK.RestSharpClient")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCompany("Stormpath, Inc.")]
+[assembly: AssemblyProduct("Stormpath .NET SDK")]
+[assembly: AssemblyCopyright("Copyright © 2015 Stormpath, Inc.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -49,6 +49,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.2.0.0")]
+[assembly: AssemblyFileVersion("0.2.0.0")]
 [assembly: InternalsVisibleTo("Stormpath.SDK.RestSharpClient.Tests")]
