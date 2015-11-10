aa5e3213dbd2cfb99f4b8c8e9f925f64389b044c
Nate Barbettini <nathanaelb@gmail.com>
2015-11-10 09:17:44 -0800

    Add Redis cache project

---
+++ Stormpath.SDK/Stormpath.SDK.Redis/RedisAsyncCache.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using StackExchange.Redis;
+using Stormpath.SDK.Cache;
+
+namespace Stormpath.SDK.Redis
+{
+    internal sealed class RedisAsyncCache<K, V> : IAsynchronousCache<K, V>
+    {
+        private readonly IConnectionMultiplexer connection;
+
+        public RedisAsyncCache(IConnectionMultiplexer connection,
+            string region,
+            CancellationToken cancellationToken)
+        {
+            var db = connection.GetDatabase();
+        }
+
+        string ICache<K, V>.Name
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        TimeSpan? ICache<K, V>.TimeToIdle
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        TimeSpan? ICache<K, V>.TimeToLive
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        void IDisposable.Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<V> IAsynchronousCache<K, V>.GetAsync(K key, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<V> IAsynchronousCache<K, V>.PutAsync(K key, V value, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<V> IAsynchronousCache<K, V>.RemoveAsync(K key, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Stormpath.SDK/Stormpath.SDK.Redis/RedisCacheProvider.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using StackExchange.Redis;
+
+namespace Stormpath.SDK.Extensions.Cache.Redis
+{
+    public class RedisCacheProvider
+    {
+        IConnectionMultiplexer redisConnection;
+
+        public RedisCacheProvider(string redisConfiguration)
+        {
+            this.redisConnection = ConnectionMultiplexer.Connect(redisConfiguration);
+        }
+    }
+}
