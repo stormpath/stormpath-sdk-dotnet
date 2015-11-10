149f1ba37d9a42fe8ec7ca477006f6c2b163a26f
Nate Barbettini <nathanaelb@gmail.com>
2015-11-10 09:35:32 -0800

    Project setup and bootstrapping

--- Stormpath.SDK/Stormpath.SDK.Redis/RedisAsyncCache.cs
+++ Stormpath.SDK/Stormpath.SDK.Redis/RedisAsyncCache.cs
@@ -1,7 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿// <copyright file="RedisAsyncCache.cs" company="Stormpath, Inc.">
+// Copyright (c) 2015 Stormpath, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+
+using System;
 using System.Threading;
 using System.Threading.Tasks;
 using StackExchange.Redis;
@@ -12,12 +25,16 @@ namespace Stormpath.SDK.Redis
     internal sealed class RedisAsyncCache<K, V> : IAsynchronousCache<K, V>
     {
         private readonly IConnectionMultiplexer connection;
+        private readonly string region;
 
-        public RedisAsyncCache(IConnectionMultiplexer connection,
-            string region,
-            CancellationToken cancellationToken)
+        public RedisAsyncCache(
+            IConnectionMultiplexer connection,
+            string region)
         {
-            var db = connection.GetDatabase();
+            this.connection = connection;
+            this.region = region;
+
+            var db = connection.GetDatabase(); // todo
         }
 
         string ICache<K, V>.Name
--- Stormpath.SDK/Stormpath.SDK.Redis/RedisCacheProvider.cs
+++ Stormpath.SDK/Stormpath.SDK.Redis/RedisCacheProvider.cs
@@ -1,20 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
+﻿// <copyright file="RedisCacheProvider.cs" company="Stormpath, Inc.">
+// Copyright (c) 2015 Stormpath, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+
+using System;
 using StackExchange.Redis;
+using Stormpath.SDK.Cache;
 
 namespace Stormpath.SDK.Extensions.Cache.Redis
 {
-    public class RedisCacheProvider
+    public class RedisCacheProvider : AbstractCacheProvider
     {
-        IConnectionMultiplexer redisConnection;
+        private readonly IConnectionMultiplexer redisConnection;
 
         public RedisCacheProvider(string redisConfiguration)
+            : base(syncSupported: true, asyncSupported: true)
         {
             this.redisConnection = ConnectionMultiplexer.Connect(redisConfiguration);
+        }
+
+        protected override IAsynchronousCache<K, V> CreateAsyncCache<K, V>(string name, TimeSpan? ttl, TimeSpan? tti)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override ISynchronousCache<K, V> CreateSyncCache<K, V>(string name, TimeSpan? ttl, TimeSpan? tti)
+        {
+            throw new NotImplementedException();
         }
     }
 }
