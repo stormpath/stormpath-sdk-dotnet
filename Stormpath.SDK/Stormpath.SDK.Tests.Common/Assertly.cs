a7d5a07c0868426fb3cc4ff8b57986ac904797cc
Nate Barbettini <nathanaelb@gmail.com>
2015-11-12 18:16:23 -0800

    Add common test helper project

---
+++ Stormpath.SDK/Stormpath.SDK.Tests.Common/Assertly.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stormpath.SDK.Tests.Common
+{
+    public static class Assertly
+    {
+        public static void Fail(string message)
+        {
+            Xunit.Assert.True(false, message);
+        }
+
+        public static void Todo()
+        {
+            Xunit.Assert.True(false, "TODO");
+        }
+    }
+}
---
+++ Stormpath.SDK/Stormpath.SDK.Tests.Common/DebugOnlyFactAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics;
+using Xunit;
+
+namespace Stormpath.SDK.Tests.Common
+{
+    public class DebugOnlyFactAttribute : FactAttribute
+    {
+        public DebugOnlyFactAttribute()
+        {
+            if (!Debugger.IsAttached)
+            {
+                Skip = "Only run in Debug mode.";
+            }
+        }
+    }
+}
