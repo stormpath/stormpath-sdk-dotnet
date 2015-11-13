a8a25b193ec3ed7bce5c6be7a3672c607b4deee0
Nate Barbettini <nathanaelb@gmail.com>
2015-11-13 11:20:48 -0800

    Add StyleCop rules to Stormpath.Demo

--- Stormpath.SDK/Stormpath.Demo/Program.cs
+++ Stormpath.SDK/Stormpath.Demo/Program.cs
@@ -1,12 +1,28 @@
-﻿using System;
+﻿// <copyright file="Program.cs" company="Stormpath, Inc.">
+// Copyright (c) 2015 Stormpath, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+
+using System;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace Stormpath.Demo
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             // Wire up the console cancel event (Ctrl+C) to cancel async tasks
             var cts = new CancellationTokenSource();
@@ -50,7 +66,7 @@ namespace Stormpath.Demo
             }
         }
 
-        static async Task RunAsync<T>(CancellationToken cancellationToken)
+        public static async Task RunAsync<T>(CancellationToken cancellationToken)
             where T : class, IDemo, new()
         {
             var demo = new T();
