7b4c035b9070c0ee4a3c1e735d636269fcbe08fa
Nate Barbettini <nathanaelb@gmail.com>
2015-11-24 14:41:15 -0800

    Fix push package script destination

--- push-packages.ps1
+++ push-packages.ps1
@@ -2,5 +2,5 @@ Write-Host "Pushing packages to MyGet..."
 
 Get-ChildItem .\nuget-pack -Filter *.nupkg | `
 Foreach-Object {
-	nuget push $_.FullName -ApiKey $env:stormpath_myget_api_key -Source https://www.my get.org/F/stormpath-sdk/api/v2/package
+	nuget push $_.FullName -ApiKey $env:stormpath_myget_api_key -Source "https://www.myget.org/F/stormpath-sdk/api/v2/package"
 }
