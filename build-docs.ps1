91dbeaf0347c527dab545c86cb325ff354769387
Nate Barbettini <nathanaelb@gmail.com>
2015-12-04 15:20:41 -0800

    Fix page path in build docs scripts

--- build-docs.ps1
+++ build-docs.ps1
@@ -7,5 +7,6 @@ Remove-Item docs\api\SearchHelp.inc.php
 Remove-Item docs\api\SearchHelp.php
 Remove-Item docs\api\LastBuild.log
 Remove-Item docs\api\Web.Config
+(Get-Content docs\api\index.html).replace('html/Introduction.htm', '/dotnet/api/html/Introduction.htm') | Set-Content docs\api\index.html
 
 Write-Host "Done! Docs available at docs\api"
