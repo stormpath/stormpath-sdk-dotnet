aec636ba65a940fd2b2f011dd6aaceceeb61bc52
Nate Barbettini <nathanaelb@gmail.com>
2016-01-12 07:54:53 -0800

    Clean up IClaimsMutator API documentation

--- Stormpath.SDK/Stormpath.SDK/Jwt/IClaimsMutator.cs
+++ Stormpath.SDK/Stormpath.SDK/Jwt/IClaimsMutator.cs
@@ -26,65 +26,65 @@ namespace Stormpath.SDK.Jwt
         where T : IClaimsMutator<T>
     {
         /// <summary>
-        /// Sets the JWT <c>iss</c> (issuer) value. A <see langword="null"/> value will remove the property from the JSON map.
+        /// Sets the Issuer (<c>iss</c>) claim. A <see langword="null"/> value will remove the claim.
         /// </summary>
-        /// <param name="iss">The JWT <c>iss</c> value or <see langword="null"/> to remove the property from the JSON map.</param>
+        /// <param name="iss">The JWT <c>iss</c> claim value or <see langword="null"/> to remove the claim.</param>
         /// <returns>This instance for method chaining.</returns>
         T SetIssuer(string iss);
 
         /// <summary>
-        /// Sets the JWT <c>sub</c> (subject) value. A <see langword="null"/> value will remove the property from the JSON map.
+        /// Sets the Subject (<c>sub</c>) claim. A <see langword="null"/> value will remove the claim.
         /// </summary>
-        /// <param name="sub">The JWT <c>sub</c> value or <see langword="null"/> to remove the property from the JSON map.</param>
+        /// <param name="sub">The JWT <c>sub</c> claim value or <see langword="null"/> to remove the claim.</param>
         /// <returns>This instance for method chaining.</returns>
         T SetSubject(string sub);
 
         /// <summary>
-        /// Sets the JWT <c>aud</c> (audience) value. A <see langword="null"/> value will remove the property from the JSON map.
+        /// Sets the Audience (<c>aud</c>) claim. A <see langword="null"/> value will remove the claim.
         /// </summary>
-        /// <param name="aud">The JWT <c>aud</c> value or <see langword="null"/> to remove the property from the JSON map.</param>
+        /// <param name="aud">The JWT <c>aud</c> claim value or <see langword="null"/> to remove the claim.</param>
         /// <returns>This instance for method chaining.</returns>
         T SetAudience(string aud);
 
         /// <summary>
-        /// Sets the JWT <c>exp</c> (expiration) timestamp. A <see langword="null"/> value will remove the property from the JSON map.
+        /// Sets the Expiration Time (<c>exp</c>) claim. A <see langword="null"/> value will remove the claim.
         /// <para>A JWT obtained after this timestamp should not be used.</para>
         /// </summary>
-        /// <param name="exp">The JWT <c>exp</c> value or <see langword="null"/> to remove the property from the JSON map.</param>
+        /// <param name="exp">The JWT <c>exp</c> claim value or <see langword="null"/> to remove the claim.</param>
         /// <returns>This instance for method chaining.</returns>
         T SetExpiration(DateTimeOffset? exp);
 
         /// <summary>
-        /// Sets the JWT <c>nbf</c> (not before) timestamp. A <see langword="null"/> value will remove the property from the JSON map.
+        /// Sets the Not Before (<c>nbf</c>) claim. A <see langword="null"/> value will remove the claim.
         /// <para>A JWT obtained before this timestamp should not be used.</para>
         /// </summary>
-        /// <param name="nbf">The JWT <c>nbf</c> value or <see langword="null"/> to remove the property from the JSON map.</param>
+        /// <param name="nbf">The JWT <c>nbf</c> claim value or <see langword="null"/> to remove the claim.</param>
         /// <returns>This instance for method chaining.</returns>
         T SetNotBeforeDate(DateTimeOffset? nbf);
 
         /// <summary>
-        /// Sets the JWT <c>iat</c> (issued at) timestamp. A <see langword="null"/> value will remove the property from the JSON map.
+        /// Sets the Issued At (<c>iat</c>) claim. A <see langword="null"/> value will remove the claim.
         /// <para>The value is the timestamp when the JWT was created.</para>
         /// </summary>
-        /// <param name="iat">The JWT <c>iat</c> value or <see langword="null"/> to remove the property from the JSON map.</param>
+        /// <param name="iat">The JWT <c>iat</c> claim value or <see langword="null"/> to remove the claim.</param>
         /// <returns>This instance for method chaining.</returns>
         T SetIssuedAt(DateTimeOffset? iat);
 
         /// <summary>
-        /// Sets the JWT <c>jti</c> (issuer) value. A <see langword="null"/> value will remove the property from the JSON map.
-        /// <para>This value is a CaSe-SenSiTiVe unique identifier for the JWT. If specified, this value MUST be assigned in a
-        /// manner that ensures that there is a negligible probability that the same value will be accidentally
-        /// assigned to a different data object. The ID can be used to prevent the JWT from being replayed.</para>
+        /// Sets the JWT ID (<c>jti</c>) claim value. A <see langword="null"/> value will remove the claim.
         /// </summary>
-        /// <param name="jti">The JWT <c>jti</c> value or <see langword="null"/> to remove the property from the JSON map.</param>
+        /// <remarks>This value is a CaSe-SenSiTiVe unique identifier for the JWT. If specified, this value MUST be assigned in a
+        /// manner that ensures that there is a negligible probability that the same value will be accidentally
+        /// assigned to a different data object. The ID can be used to prevent the JWT from being replayed.</remarks>
+        /// <param name="jti">The JWT <c>jti</c> claim value or <see langword="null"/> to remove the claim.</param>
         /// <returns>This instance for method chaining.</returns>
         T SetId(string jti);
 
         /// <summary>
-        /// Sets a custom JWT Claims parameter value.  A <see langword="null"/> value will remove the property from the JSON map.
+        /// Sets a custom claim value.  A <see langword="null"/> value will remove the claim.
         /// </summary>
-        /// <param name="claimName">The JWT Claims property name</param>
-        /// <param name="value">the value to set for the specified Claims property name</param>
+        /// <param name="claimName">The claim name.</param>
+        /// <param name="value">The claim value.</param>
         /// <returns>This instance for method chaining.</returns>
         T SetClaim(string claimName, object value);
     }
