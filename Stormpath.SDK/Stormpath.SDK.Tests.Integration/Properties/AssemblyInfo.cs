f33c4b8b5d489cb210565150e6144ece5300e087
Nate Barbettini <nathanaelb@gmail.com>
2016-01-15 15:26:26 -0800

    Version bump for 0.4.1

--- Stormpath.SDK/Stormpath.SDK.Tests.Integration/Properties/AssemblyInfo.cs
+++ Stormpath.SDK/Stormpath.SDK.Tests.Integration/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("09b79235-d67f-4fca-9991-5ae30838eece")]
 
-[assembly: AssemblyVersion("0.4.0.0")]
-[assembly: AssemblyFileVersion("0.4.0.0")]
+[assembly: AssemblyVersion("0.4.1.0")]
+[assembly: AssemblyFileVersion("0.4.1.0")]
