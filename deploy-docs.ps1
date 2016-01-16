c867a38b4802aca93e6dcce9416ffdcedf8174cd
Nate Barbettini <nathanaelb@gmail.com>
2016-01-15 18:30:44 -0800

    Add more log output to autodeploy script, final tweaks

--- deploy-docs.ps1
+++ deploy-docs.ps1
@@ -1,5 +1,4 @@
 $built = $false
-$built = $true # remove!
 
 if ($env:build_docs)
 {
@@ -30,21 +29,28 @@ if (!$deploy_docs) {
 		Write-Host "Tagged release $($latestTag) found"
 		$deploy_docs = 1
 	}
+	else
+	{
+		Write-Host "Not a tagged release, will not deploy documentation."
+	}
 }
 
 if ($built -and $deploy_docs) {
 	Write-Host "Deploying documentation!"
 	
+	Write-Host "Cloning stormpath/stormpath.github.io"
 	git clone -q git@github.com:stormpath/stormpath.github.io.git --branch source 2> $null
 	cd stormpath.github.io
 	git config user.email "evangelists@stormpath.com"
 	git config user.name "sdk-dotnet Auto Doc Build"
 	
 	$branchName = "dotnet-autodeploy-$($latestTag)"
-	git checkout -b $branchName
+	Write-Host "Creating branch $($branchName)"
+	git checkout -b $branchName 2> $null
 	
+	Write-Host "Copying new files and pushing to repository"
 	Copy-Item ..\docs\api source\dotnet\ -recurse -force
-	git add --all
+	git add --all 
 	git commit -m "sdk-dotnet release $($latestTag)"
 	git push -u origin $branchName
 }
