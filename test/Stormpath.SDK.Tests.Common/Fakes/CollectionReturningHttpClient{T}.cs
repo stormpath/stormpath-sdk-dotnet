27b40acbef5ecf8fb0ebda94007bd853e2f674c5
Nate Barbettini <nathanaelb@gmail.com>
2016-02-12 16:27:01 -0800

    Add common test project

--- test/Stormpath.SDK.Tests.Common/Fakes/CollectionReturningHttpClient{T}.cs
+++ test/Stormpath.SDK.Tests.Common/Fakes/CollectionReturningHttpClient{T}.cs
@@ -22,6 +22,7 @@ using Stormpath.SDK.Http;
 using Stormpath.SDK.Impl.DataStore;
 using Stormpath.SDK.Impl.Extensions;
 using Stormpath.SDK.Impl.Resource;
+using Stormpath.SDK.Shared.Extensions;
 using Map = System.Collections.Generic.IDictionary<string, object>;
 
 namespace Stormpath.SDK.Tests.Common.Fakes
