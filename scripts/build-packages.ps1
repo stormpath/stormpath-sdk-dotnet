1cb00f2ee839acd50ca279fe53b3b6c3a2e5b019
Nate Barbettini <nathanaelb@gmail.com>
2016-02-19 14:51:31 -0800

    Update package build script to use dnu pack

--- scripts/build-packages.ps1
+++ scripts/build-packages.ps1
@@ -1,8 +1,20 @@
-Write-Host "Packing nuspec files..."
-$currentDir = (Resolve-Path .\).Path
-$createResult = md -Force ($currentDir + "\nuget-pack")
+param([switch]$local = $false)
 
-Get-ChildItem $currentDir -Filter *.nuspec | `
-Foreach-Object {
-	nuget pack $_.FullName -OutputDirectory nuget-pack
+$sourceFolder = "{0}\..\src" -f $PSScriptRoot
+
+Write-Host "Packaging projects..."
+dnu pack ("{0}\Stormpath.SDK.Abstractions" -f $sourceFolder)
+dnu pack ("{0}\Stormpath.SDK.Core" -f $sourceFolder)
+dnu pack ("{0}\Stormpath.SDK.Http.SystemNetHttpClient" -f $sourceFolder)
+dnu pack ("{0}\Stormpath.SDK.JsonNetSerializer" -f $sourceFolder)
+dnu pack ("{0}\Stormpath.SDK.Redis" -f $sourceFolder)
+dnu pack ("{0}\Stormpath.SDK.RestSharpClient" -f $sourceFolder)
+
+if ($local) {
+	Write-Host "Adding to local NuGet server..."
+
+	Get-ChildItem $sourceFolder -Recurse -Filter *.nupkg | `
+	ForEach-Object {
+		nuget add $_.FullName -Source $env:local_nuget_path
+	}
 }
