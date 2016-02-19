3ce381e3a27754b760f319469c33807b339d37f6
Nate Barbettini <nathanaelb@gmail.com>
2016-02-18 16:06:40 -0800

    Replace old doc build script

--- scripts/build-docs.ps1
+++ scripts/build-docs.ps1
@@ -1,22 +1,40 @@
 $exitCode = 0
 
+Write-Host "Generating proxy .csproj files..."
+
+# Import proxy generation function
+. ("{0}\generate-doc-proxy.ps1" -f $PSScriptRoot)
+
+try
+{
+  Generate-Documentation-Proxy "docs\proxy\Stormpath.SDK.Abstractions\Stormpath.SDK.Abstractions.csproj" "src\Stormpath.SDK.Abstractions"
+  Generate-Documentation-Proxy "docs\proxy\Stormpath.SDK\Stormpath.SDK.csproj" "src\Stormpath.SDK"
+}
+catch
+{
+  $exitCode = 1
+  throw
+}
+
 Write-Host "Compiling documentation..."
+
 & msbuild docs\StormpathSDKApiDocs.shfbproj /verbosity:minimal /nologo
 $exitCode = $LASTEXITCODE
 
 Write-Host "Cleaning up files..."
-Try
+
+try
 {
-	Remove-Item docs\api\SearchHelp.aspx -ErrorAction Stop
-	Remove-Item docs\api\SearchHelp.inc.php -ErrorAction Stop
-	Remove-Item docs\api\SearchHelp.php -ErrorAction Stop
-	Remove-Item docs\api\LastBuild.log -ErrorAction Stop
-	Remove-Item docs\api\Web.Config -ErrorAction Stop
-	(Get-Content docs\api\index.html -ErrorAction Stop).replace('html/Introduction.htm', '/dotnet/api/html/Introduction.htm') | Set-Content docs\api\index.html -ErrorAction Stop
+	Remove-Item "docs\api\SearchHelp.aspx" -ErrorAction Stop
+	Remove-Item "docs\api\SearchHelp.inc.php" -ErrorAction Stop
+	Remove-Item "docs\api\SearchHelp.php" -ErrorAction Stop
+	Remove-Item "docs\api\LastBuild.log" -ErrorAction Stop
+	Remove-Item "docs\api\Web.Config" -ErrorAction Stop
+	(Get-Content "docs\api\index.html" -ErrorAction Stop).replace('html/Introduction.htm', '/dotnet/api/html/Introduction.htm') | Set-Content "docs\api\index.html" -ErrorAction Stop
 }
-Catch
+catch
 {
 	$exitCode = 1
 }
 
-Return $exitCode
+return $exitCode
--- scripts/build-docs2.ps1
+++
@@ -1,4 +0,0 @@
-. ("{0}\generate-doc-proxy.ps1" -f $PSScriptRoot)
-
-Generate-Documentation-Proxy "docs\proxy\Stormpath.SDK.Abstractions\Stormpath.SDK.Abstractions.csproj" "src\Stormpath.SDK.Abstractions"
-Generate-Documentation-Proxy "docs\proxy\Stormpath.SDK\Stormpath.SDK.csproj" "src\Stormpath.SDK"
