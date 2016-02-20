93799cb380034262a3a9b101a119fa4b8e4be05b
Nate Barbettini <nathanaelb@gmail.com>
2016-02-19 16:24:25 -0800

    Update doc deploy script with new paths

--- script/deploy-docs.ps1
+++ script/deploy-docs.ps1
@@ -1,4 +1,4 @@
-$built = $false
+$built = $true
 
 if ($env:build_docs)
 {
@@ -24,7 +24,7 @@ $deploy_docs = $env:deploy_docs
 $latestTag = git tag | tail -n 1
 if (!$deploy_docs) {
 	Write-Host "deploy_docs not set; checking for tagged release"
-	
+
 	if ((git log --decorate --oneline | head -n 1).contains("HEAD, tag: " + $latestTag)) {
 		Write-Host "Tagged release $($latestTag) found"
 		$deploy_docs = 1
@@ -37,20 +37,20 @@ if (!$deploy_docs) {
 
 if ($built -and $deploy_docs) {
 	Write-Host "Deploying documentation!"
-	
+
 	Write-Host "Cloning stormpath/stormpath.github.io"
 	git clone -q git@github.com:stormpath/stormpath.github.io.git --branch source 2> $null
 	cd stormpath.github.io
 	git config user.email "evangelists@stormpath.com"
 	git config user.name "sdk-dotnet Auto Doc Build"
-	
+
 	$branchName = "dotnet-autodeploy-$($latestTag)"
 	Write-Host "Creating branch $($branchName)"
 	git checkout -b $branchName 2> $null
-	
+
 	Write-Host "Copying new files and pushing to repository"
-	Copy-Item ..\docs\api source\dotnet\ -recurse -force
-	git add --all 
+	Copy-Item ..\doc\api source\dotnet\ -recurse -force
+	git add --all
 	git commit -m "sdk-dotnet release $($latestTag)"
 	git push -u origin $branchName
 }
