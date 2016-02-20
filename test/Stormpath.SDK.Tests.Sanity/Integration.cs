c7bca7668f3cf2e640caa52db32cdef9accecf1f
Nate Barbettini <nathanaelb@gmail.com>
2016-02-19 18:05:20 -0800

    Fix silly unit test failures

--- test/Stormpath.SDK.Tests.Sanity/Integration.cs
+++ test/Stormpath.SDK.Tests.Sanity/Integration.cs
@@ -21,9 +21,8 @@ namespace Stormpath.SDK.Tests.Sanity
 {
     public class Integration
     {
-        //[Fact(Skip = "Restore VB sanity tests.")]
+        [Fact(Skip = "Restore VB sanity tests.")]
         [Obsolete("Restore VB sanity tests.")]
-        [Fact]
         public void Notice()
         {
             Assert.False(true);
