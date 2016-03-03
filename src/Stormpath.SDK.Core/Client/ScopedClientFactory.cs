04d1f4e633030cd46466bd7b08e16a4d0519c24a
Nate Barbettini <nathanaelb@gmail.com>
2016-03-02 17:57:58 -0800

    Add ScopedClientFactory and refactor some internals

---
+++ src/Stormpath.SDK.Core/Client/ScopedClientFactory.cs
@@ -0,0 +1,53 @@
+﻿// <copyright file="ScopedClientOptions.cs" company="Stormpath, Inc.">
+// Copyright (c) 2016 Stormpath, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Stormpath.SDK.Impl.Client;
+
+namespace Stormpath.SDK.Client
+{
+    public sealed class ScopedClientFactory : IScopedClientFactory
+    {
+        private readonly DefaultClient baseClient;
+
+        public ScopedClientFactory(IClient existing)
+        {
+            this.baseClient = existing as DefaultClient;
+
+            if (this.baseClient == null)
+            {
+                throw new ArgumentException("Unknown client type.", nameof(existing));
+            }
+        }
+
+        public IClient Create(ScopedClientOptions options = null)
+        {
+            if (options == null)
+            {
+                options = new ScopedClientOptions();
+            }
+
+            // Create a new user agent builder
+            var userAgentBuilder = baseClient.UserAgentBuilder;
+            //todo
+
+            return new DefaultClient(this.baseClient, userAgentBuilder, options.Identifier);
+        }
+    }
+}
