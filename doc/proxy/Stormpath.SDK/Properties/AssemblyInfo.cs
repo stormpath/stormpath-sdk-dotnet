f353e1421519b9dfb9aa0f5491427536cc0688dc
Nate Barbettini <nathanaelb@gmail.com>
2016-03-11 13:18:57 -0800

    Bump doc version to 0.7.1

--- doc/proxy/Stormpath.SDK/Properties/AssemblyInfo.cs
+++ doc/proxy/Stormpath.SDK/Properties/AssemblyInfo.cs
@@ -4,6 +4,6 @@ using System.Reflection;
 [assembly: AssemblyDescription("The official Stormpath SDK for .NET. Don't install this package directly; install the Stormpath.SDK package unless you are bringing your own JSON serializer and HTTP client plugins.")]
 [assembly: AssemblyCompany("Stormpath, Inc.")]
 [assembly: AssemblyCopyright("(c) 2016 Stormpath, Inc.")]
-[assembly: AssemblyVersion("0.7.0")]
-[assembly: AssemblyFileVersion("0.7.0")]
+[assembly: AssemblyVersion("0.7.1")]
+[assembly: AssemblyFileVersion("0.7.1")]
 
