87c027fae6b8ba6bd34f7a230522eb8f402ce48b
Nate Barbettini <nathanaelb@gmail.com>
2016-05-17 09:29:13 -0700

    Update SDK docs

--- doc/proxy/Stormpath.SDK.Abstractions/Properties/AssemblyInfo.cs
+++ doc/proxy/Stormpath.SDK.Abstractions/Properties/AssemblyInfo.cs
@@ -4,6 +4,6 @@ using System.Reflection;
 [assembly: AssemblyDescription("Abstractions for the Stormpath .NET SDK.")]
 [assembly: AssemblyCompany("Stormpath, Inc.")]
 [assembly: AssemblyCopyright("(c) 2016 Stormpath, Inc.")]
-[assembly: AssemblyVersion("0.8.3")]
-[assembly: AssemblyFileVersion("0.8.3")]
+[assembly: AssemblyVersion("0.9.0")]
+[assembly: AssemblyFileVersion("0.9.0")]
 
