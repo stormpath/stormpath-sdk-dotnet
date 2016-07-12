68d1b5b834f3e8d19a43034979c7524f58bbee56
Nate Barbettini <nathanaelb@gmail.com>
2016-07-11 17:22:25 -0700

    CDS documentation examples

--- doc/examples/DocExamples/ProductGuide/Account_Management.cs
+++ doc/examples/DocExamples/ProductGuide/Account_Management.cs
@@ -194,6 +194,23 @@ namespace DocExamples.ProductGuide
             #endregion
         }
 
+        public async Task CustomDataSearchStartDate()
+        {
+            IDirectory myDirectory = null;
+
+            #region cd_search.cs
+            var windowStart = new DateTimeOffset(2012, 01, 01, 00, 00, 00, TimeSpan.Zero);
+            var windowEnd = new DateTimeOffset(2015, 12, 31, 23, 59, 59, TimeSpan.Zero);
+
+            var accountsStartedBetween2012and2015 = await myDirectory
+                .GetAccounts()
+                .Where(a => (DateTimeOffset)a.CustomData["startDate"] >= windowStart
+                         && (DateTimeOffset)a.CustomData["startDate"] <= windowEnd)
+                .Take(5)
+                .ToListAsync();
+            #endregion
+        }
+
         public async Task UpdateAccountPassword()
         {
             IAccount picard = null;
--- doc/examples/DocExamplesVB/ProductGuide/Account_Management.vb
+++ doc/examples/DocExamplesVB/ProductGuide/Account_Management.vb
@@ -158,6 +158,22 @@ Public Class Account_Management
             .ToListAsync() ' Jan 5 2016, midnight GMT
 #End Region
     End Function
+    
+    Public Async Function CustomDataSearchStartDate() As Task
+	    Dim myDirectory As IDirectory = Nothing
+
+#Region "cd_search.vb"
+        Dim windowStart = New DateTimeOffset(2012, 1, 1, 0, 0, 0, TimeSpan.Zero)
+        Dim windowEnd = New DateTimeOffset(2015, 12, 31, 23, 59, 59, TimeSpan.Zero)
+
+        Dim accountsStartedBetween2012and2015 = Await myDirectory _
+            .GetAccounts() _
+            .Where(Function(a) a.CustomData("startDate") >= windowStart _
+                       AndAlso a.CustomData("startDate") <= windowEnd) _
+            .Take(5) _
+            .ToListAsync()
+#End Region
+    End Function
 
     Public Async Function UpdateAccountPassword() As Task
         Dim picard As IAccount = Nothing
