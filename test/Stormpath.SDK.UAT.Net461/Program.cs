cff5b455c8adbe3c980377e7008cdc0a71630e0d
Nate Barbettini <nathanaelb@gmail.com>
2016-09-01 17:05:30 -0700

    Add net461 testing project

---
+++ test/Stormpath.SDK.UAT.Net461/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Stormpath.SDK.Client;
+using Stormpath.SDK.Http;
+using Stormpath.SDK.Serialization;
+
+namespace Stormpath.SDK.UAT.Net461
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            TestRestSharpAdapterCreation();
+
+            Console.WriteLine("All net461 UATs done");
+        }
+
+        private static void TestRestSharpAdapterCreation()
+        {
+            var client = Clients.Builder()
+                .SetHttpClient(HttpClients.Create().RestSharpClient())
+                .SetSerializer(Serializers.Create().JsonNetSerializer())
+                .SetApiKeyId("fake")
+                .SetApiKeySecret("reallyfake")
+                .Build();
+        }
+    }
+}
