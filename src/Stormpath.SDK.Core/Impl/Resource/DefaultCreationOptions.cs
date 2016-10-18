88caf626b9e1130bdbf640e61c655f27e5163d7a
Nate Barbettini <nathanaelb@gmail.com>
2016-10-17 18:11:12 -0700

    Add a generic class for creation parameters

---
+++ src/Stormpath.SDK.Core/Impl/Resource/DefaultCreationOptions.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Stormpath.SDK.Resource;
+
+namespace Stormpath.SDK.Impl.Resource
+{
+    internal sealed class DefaultCreationOptions : ICreationOptions
+    {
+        private readonly KeyValuePair<string, string>[] _parameters;
+
+        public DefaultCreationOptions(IEnumerable<KeyValuePair<string, string>> parameters)
+        {
+            _parameters = parameters?.ToArray() ?? new KeyValuePair<string, string>[0];
+        }
+
+        public string GetQueryString()
+        {
+            if (!_parameters.Any())
+            {
+                return null;
+            }
+
+            return string.Join("&", _parameters.Select(kvp => $"{kvp.Key}={kvp.Value}"));
+        }
+    }
+}
---
+++ test/Stormpath.SDK.Tests/DefaultCreationOptionsShould.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using Stormpath.SDK.Impl.Resource;
+using Xunit;
+
+namespace Stormpath.SDK.Tests
+{
+    public class DefaultCreationOptionsShould
+    {
+        [Fact]
+        public void ReturnNullForNoParameters()
+        {
+            var options = new DefaultCreationOptions(null);
+
+            options.GetQueryString().Should().BeNullOrEmpty();
+        }
+
+        [Fact]
+        public void ReturnSingleParameter()
+        {
+            var options = new DefaultCreationOptions(new Dictionary<string, string>
+            {
+                ["challenge"] = "true"
+            });
+
+            options.GetQueryString().Should().Be("challenge=true");
+        }
+
+        [Fact]
+        public void ReturnMultipleParameters()
+        {
+            var options = new DefaultCreationOptions(new Dictionary<string, string>
+            {
+                ["foo"] = "bar",
+                ["baz"] = "123"
+            });
+
+            options.GetQueryString().Should().Be("foo=bar&baz=123");
+        }
+    }
+}
