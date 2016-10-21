af37f4c7f2638302887d9361d0d7292544a90832
Nate Barbettini <nathanaelb@gmail.com>
2016-10-21 14:54:40 -0700

    Add password grant request matching new style

---
+++ test/Stormpath.SDK.Tests/Oauth/PasswordGrantRequestShould.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FluentAssertions;
+using NSubstitute;
+using Stormpath.SDK.Application;
+using Stormpath.SDK.Http;
+using Stormpath.SDK.Oauth;
+using Stormpath.SDK.Sync;
+using Stormpath.SDK.Tests.Common.Fakes;
+using Stormpath.SDK.Tests.Helpers;
+using Xunit;
+
+namespace Stormpath.SDK.Tests.Oauth
+{
+    public class PasswordGrantRequestShould
+    {
+        [Fact]
+        public async Task MatchExpectedSyntaxAsync()
+        {
+            var dataStore = TestDataStore.Create(FakeJson.Application);
+
+            var application = await dataStore.GetResourceAsync<IApplication>("http://myapp");
+
+            await application.ExecuteOauthRequestAsync(new PasswordGrantRequest
+            {
+                Username = "tom@stormpath.com",
+                Password = "Secret123!!"
+            });
+
+            var call = dataStore.RequestExecutor.ReceivedCalls().Last();
+            var httpRequest = call.GetArguments()[0] as IHttpRequest;
+            ValidateParameters(httpRequest);
+        }
+
+        [Fact]
+        public void MatchExpectedSyntax()
+        {
+            var dataStore = TestDataStore.Create(FakeJson.Application);
+
+            var application = dataStore.GetResource<IApplication>("http://myapp");
+
+            application.ExecuteOauthRequest(new PasswordGrantRequest
+            {
+                Username = "tom@stormpath.com",
+                Password = "Secret123!!"
+            });
+
+            var call = dataStore.RequestExecutor.ReceivedCalls().Last();
+            var httpRequest = call.GetArguments()[0] as IHttpRequest;
+            ValidateParameters(httpRequest);
+        }
+
+        private static void ValidateParameters(IHttpRequest request)
+        {
+            request.CanonicalUri.ToString().Should().EndWith("/oauth/token");
+            request.BodyContentType.Should().Be("application/x-www-form-urlencoded");
+
+            var requestBody = request.Body.Split('&');
+            requestBody.Should().Contain("grant_type=password");
+            requestBody.Should().Contain("username=tom%40stormpath.com");
+            requestBody.Should().Contain("password=Secret123%21%21");
+        }
+    }
+}
